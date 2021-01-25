f5fa96a459175698bbe59f44504a3819608fe8df
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-25 10:06:20 +0600

    event delegates

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 
-document.getElementById('firstButton3').addEventListener('click', function(){
-    document.body.style.backgroundColor = 'teal';
+// document.getElementById('firstButton3').addEventListener('click', function(){
+//     document.body.style.backgroundColor = 'teal';
+// });
+
+// document.getElementById('first').addEventListener('click', function(event){
+//  console.log('clicked on first');
+// });
+
+
+var items = document.getElementsByClassName('item');
+for (let i=0; i<items.length; i++){
+    var item= items[i];
+    item.addEventListener('click', function(event){
+        // event.target.parentNode.removeChild(event.target);
+    });
+}
+
+document.getElementById('addNew').addEventListener('click', function(){
+ var itemParent = document.getElementById('myList');
+ var newItem = document.createElement('li');
+ newItem.innerText ='Brand new item';
+ itemParent.appendChild(newItem)
 });
